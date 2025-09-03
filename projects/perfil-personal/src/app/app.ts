d7c74751c0953f964a1ee13348f66fc455a2d9ed
Tatoh <tatukis@gmail.com>
2025-09-03 11:23:41 -0300

    feat: add sidebar component with basic structure and styles; update app configuration and global styles

--- projects/perfil-personal/src/app/app.ts
+++ projects/perfil-personal/src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Avatar } from 'componentes';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Avatar],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
